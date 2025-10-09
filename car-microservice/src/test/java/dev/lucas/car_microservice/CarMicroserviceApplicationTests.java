7252b6995d117fc48cba7bc8bc5bf31e9ac3b53b
lucas <lucasaita4000@gmail.com>
2025-10-09 03:16:02 -0300

    Initial Commit

---
+++ car-microservice/src/test/java/dev/lucas/car_microservice/CarMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lucas.car_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ user-microservice/src/test/java/dev/lucas/user_microservice/UserMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lucas.user_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
