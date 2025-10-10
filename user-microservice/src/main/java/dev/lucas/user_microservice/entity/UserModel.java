157ae028df92abebeaffca41740c24a8a0e851f2
lucas <lucasaita4000@gmail.com>
2025-10-10 04:15:41 -0300

    feat: implementa a interface UserDetails para dentro do nosso Model, sendo possível trabalhar com autenticacao e seguranca

--- user-microservice/src/main/java/dev/lucas/user_microservice/entity/UserModel.java
+++ user-microservice/src/main/java/dev/lucas/user_microservice/entity/UserModel.java
@@ -5,6 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "TB_USERS")
@@ -12,7 +17,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserModel {
+public class UserModel implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,4 +27,34 @@ public class UserModel {
     private String cpf;
     private String cnh;
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
