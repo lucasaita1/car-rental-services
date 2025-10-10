0f85d9a0e0b3dcf41772b4274a6ddbde29d5056c
lucas <lucasaita4000@gmail.com>
2025-10-10 04:25:19 -0300

    feat: cria um service responsavel por autenticar o login e validar o email e senha fornecidos pelo usuário

---
+++ user-microservice/src/main/java/dev/lucas/user_microservice/service/AuthService.java
@@ -0,0 +1,21 @@
+package dev.lucas.user_microservice.service;
+
+import dev.lucas.user_microservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found! \n Email or password inaccurate"));
+    }
+}
