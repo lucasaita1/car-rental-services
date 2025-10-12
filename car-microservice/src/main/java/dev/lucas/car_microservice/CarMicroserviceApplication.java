903b9f33cde449691125ef0bc11660f584493113
lucas <lucasaita4000@gmail.com>
2025-10-11 22:59:51 -0300

    build: traz o dot env para dentro do nosso application para carregar as variaveis de ambiente automaticamente

--- car-microservice/src/main/java/dev/lucas/car_microservice/CarMicroserviceApplication.java
+++ car-microservice/src/main/java/dev/lucas/car_microservice/CarMicroserviceApplication.java
@@ -1,13 +1,17 @@
 package dev.lucas.car_microservice;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CarMicroserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CarMicroserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // Carrega as variáveis do .env antes do Spring inicializar
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
+        SpringApplication.run(CarMicroserviceApplication.class, args);
+    }
 }
