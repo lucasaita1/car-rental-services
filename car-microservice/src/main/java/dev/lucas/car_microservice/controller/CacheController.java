fc42f73cc322df7d053ca3fec24fa7bd3301b04b
lucas <lucasaita4000@gmail.com>
2025-10-14 01:20:31 -0300

    fix: corrige o Cache Controller para ser somente o ponto de conexao entre os servicos, desacomplando as responsabilidades de salvar e listar dados para o service

--- car-microservice/src/main/java/dev/lucas/car_microservice/controller/CacheController.java
+++ car-microservice/src/main/java/dev/lucas/car_microservice/controller/CacheController.java
@@ -2,27 +2,28 @@ package dev.lucas.car_microservice.controller;
 
 
 import dev.lucas.car_microservice.dto.UserCacheDto;
+import dev.lucas.car_microservice.service.CacheService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.Duration;
 
 @RestController
 @RequestMapping("/cache")
 @RequiredArgsConstructor
 public class CacheController {
 
-    private final RedisTemplate<String, Object> redisTemplate;
+    private final CacheService cacheService;
 
     @PostMapping("/user")
     public String saveUserCache(@RequestBody UserCacheDto userCacheDto) {
-        // Salva no Redis com uma chave única
-        String key = "user:" + userCacheDto.getId();
-        redisTemplate.opsForValue().set(key, userCacheDto);
+        cacheService.saveUser(userCacheDto, Duration.ofMinutes(120));
         return "Cache de usuário salvo com sucesso no Redis!";
     }
 
     @GetMapping("/user/{id}")
-    public Object getUserCache(@PathVariable String id) {
-        return redisTemplate.opsForValue().get("user:" + id);
+    public UserCacheDto getUserCache(@PathVariable String id) {
+        return cacheService.getUser(id); // retorna o DTO completo
     }
 }
