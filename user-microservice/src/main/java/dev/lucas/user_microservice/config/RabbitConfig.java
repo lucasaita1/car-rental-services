097e38872864841f66513b3a1154467eb3b5d5ed
lucas <lucasaita4000@gmail.com>
2025-10-20 17:26:35 -0300

    feat: adiciona uma classe de configuracao do RabbitMQ para o Spring gerenciar

---
+++ user-microservice/src/main/java/dev/lucas/user_microservice/config/RabbitConfig.java
@@ -0,0 +1,14 @@
+package dev.lucas.user_microservice.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+}
