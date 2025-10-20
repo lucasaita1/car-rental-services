b3b5fe9cf28cef6c32caeb809abc0bc6fe1866ff
lucas <lucasaita4000@gmail.com>
2025-10-20 17:20:50 -0300

    fix: corrige os imports desnecessários

--- user-microservice/src/main/java/dev/lucas/user_microservice/config/SecurityFilterConfig.java
+++ user-microservice/src/main/java/dev/lucas/user_microservice/config/SecurityFilterConfig.java
@@ -5,7 +5,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.apache.logging.log4j.util.Strings;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
