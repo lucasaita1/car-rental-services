771597615c869d538fa43f6dae91a9dff71044f7
lucas <lucasaita4000@gmail.com>
2025-10-20 17:28:23 -0300

    feat: configura o nosso producer do servico de usuarios, sendo possivel enviar informacoes do cadastro desse usuario para o RabbitMQ, faltando somente configurar o servico de email que sera o consumer dessas informacoes.

---
+++ user-microservice/src/main/java/dev/lucas/user_microservice/producer/UserProducer.java
@@ -0,0 +1,25 @@
+package dev.lucas.user_microservice.producer;
+
+import dev.lucas.user_microservice.dtos.EmailDto;
+import dev.lucas.user_microservice.entity.UserModel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class UserProducer {
+
+    private final RabbitTemplate rabbitTemplate;
+    private final String routingKey = "register_email";
+
+  public void sendRegisterEmail(UserModel user) {
+      var email = new EmailDto();
+      email.setUserId(user.getId());
+      email.setEmailTo(user.getEmail());
+      email.setSubject("Register Email");
+      email.setText("Olá " + user.getUsername() + ", obrigado por se cadastrar em nosso sistema. Esta é uma mensagem automática de boas-vindas.");
+
+      rabbitTemplate.convertAndSend("", routingKey, email);
+  }
+}
