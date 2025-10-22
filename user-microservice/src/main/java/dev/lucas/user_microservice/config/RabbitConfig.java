3966092d4646d5678c687cfa1becc9a006f67a9d
lucas <lucasaita4000@gmail.com>
2025-10-22 04:51:25 -0300

    fix: corrige a falta de um @Bean e com isso tornando o Spring capaz de gerenciar o a conversao de Objetos

--- user-microservice/src/main/java/dev/lucas/user_microservice/config/RabbitConfig.java
+++ user-microservice/src/main/java/dev/lucas/user_microservice/config/RabbitConfig.java
@@ -2,11 +2,13 @@ package dev.lucas.user_microservice.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitConfig {
 
+    @Bean
     public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
         ObjectMapper objectMapper = new ObjectMapper();
         return new Jackson2JsonMessageConverter(objectMapper);
