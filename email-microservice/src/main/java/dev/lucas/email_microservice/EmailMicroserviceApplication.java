83a1f2b568ce013a35aafc52df1eed8dfe87f681
lucas <lucasaita4000@gmail.com>
2025-10-22 04:48:12 -0300

    build: traz anotacao de @EnableRabbit para ser inicialiazada a conexao

--- email-microservice/src/main/java/dev/lucas/email_microservice/EmailMicroserviceApplication.java
+++ email-microservice/src/main/java/dev/lucas/email_microservice/EmailMicroserviceApplication.java
@@ -1,12 +1,23 @@
 package dev.lucas.email_microservice;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableRabbit
 public class EmailMicroserviceApplication {
 
 	public static void main(String[] args) {
+        //carrega as variaveis de ambiente do .env
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry ->
+                        System.setProperty(entry.getKey(), entry.getValue())
+        );
 		SpringApplication.run(EmailMicroserviceApplication.class, args);
 	}
 
